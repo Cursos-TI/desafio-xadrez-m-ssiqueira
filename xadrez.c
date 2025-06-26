0f9bece4ac6ea920053961364153fa73a468f677
m-ssiqueira <mssiqueira@gmail.com>
2025-06-26 14:51:56 +0000

    Trabalho Desafio Xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,145 @@
+// exervivio de movimentação de xadrez.
+
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+//movimento da TORRE - CIMA
+void mover_torre (int casas, char posX, int posY){
+    if (casas > 0 ){
+        printf("Frente %c%d\n", posX, ++posY);
+        mover_torre (casas -1, posX, posY);
+    }
 }
+//movimento do BISPO - DIREITA FRENTE
+void mover_bispo (int casas, char posX, int posY){
+    if (casas > 0){
+        printf("Direita, Frente, %c,%d.\n", ++posX, ++posY);
+        mover_bispo(casas -1, posX, posY);
+    }
+}
+// movimento da RAINHA - ESQUERDA
+void mover_rainha (int casas, char posX, int posY){
+    if (casas > 0){
+        printf("Esquerda, %c,%d.\n", --posX, posY);
+        mover_rainha(casas -1, posX, posY);
+    }
+}
+// movimento do CAVALO - CIMA CIMA DIREITA
+void mover_cavalo (int casas, char posX, int posY){
+    if (casas >0 ){
+        for (int i=0;i < 2; i++){
+            printf("Cima, %c,%d.\n", posX, ++posY);
+        }
+        printf("Direita %c,%d.\n", ++posX, posY);
+        mover_cavalo (casas -1, posX, posY);
+    }
+                                
+    
+}
+
+int main(){
+
+
+    int opcao, move, posY;
+    char posX;
+    /*
+    Menu Interativo;
+    Escolhe a peça e danda com ela; 
+    Não guarda a posiçao.
+    */
+    do {
+        printf("Movimente as Peças.\n");
+        printf("1 - Torre\n");
+        printf("2 - Bispo\n");
+        printf("3 - Rainha\n");
+        printf("4 - Cavalo\n");
+        printf("5 - sair do jogo\n");
+        printf("escolha: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+
+            case 1:
+                // considerando a torre começando na posição a,1 e ela podendo mover apenas pra frente.                        
+                posY = 1;
+                posX = 'a';
+                printf("Quantas casas deseja andar com a TORRE ?");
+                scanf("%d", &move);
+                if (move<8){
+                    /* 
+                        for (int i=0;i < move; i++){
+                            printf("Frente , %c,%d.\n", posX, ++posY);}
+                    */
+                    mover_torre(move, posX, posY);
+                }else {
+                        printf("Opção invalida.\n");
+                }
+                    
+            break;
+            case 2:
+                // considerando a bispo começando na posição a,1 e ela podendo mover apenas pra frente/direita                      
+                posY = 1;
+                posX = 'a';
+                printf("Quantas casas deseja andar com o Bispo ?");
+                scanf("%d", &move);
+                    if (move < 8){ 
+                    /*   
+                        for (int i=0;i < move; i++){
+                            printf("Frente, Direita, %c,%d.\n", ++posX, ++posY);}
+                    */
+                    mover_bispo (move,posX,posY);
+                    }else {
+                        printf("Opção invalida.\n");
+                    }
+                break;
+            case 3:
+                // considerando a Rainha começando na posição h,1 e ela podendo mover apenas pra esquerda                     
+                posY = 1;
+                posX = 'h';
+                printf("Quantas casas deseja andar com A Rainha ?");
+                scanf("%d", &move);
+                    if (move<8){    
+                      /* 
+                       for (int i=0;i < move; i++){
+                            printf("Esquerda, %c,%d.\n", --posX, posY);}
+                      */
+                    mover_rainha (move, posX, posY);  
+                    }else {
+                        printf("Opção invalida.\n");
+                    }
+                break;
+
+                case 4:
+                // considerando o cavalo começando na posição a,1 e ele podendo mover apenas duas vezes pra cima e uma vez pra direita  fazendo um L                  
+                posY = 1;
+                posX = 'a';
+                printf("Quantas casas deseja andar com O Cavalo ?");
+                scanf("%d", &move);
+                    if (move<8){    
+                        /*
+                        int cavalo = move;
+                        while (cavalo--){
+                            for (int i=0;i < 2; i++){
+                                printf("Cima, %c,%d.\n", posX, ++posY);
+                            }
+                            printf("Direita %c,%d.\n", ++posX, posY);}
+                        */    
+                        mover_cavalo(move, posX, posY);
+                    }else {
+                        printf("Opção invalida.\n");
+                    }
+                break;
+
+            case 5:
+                printf("Saindo do jogo.\n");
+                break;
+            default :    
+            printf("Opção invalida\n");
+        }
+    }while (opcao != 5);
+    
+        printf("Obrigado por jogar.\n");
+    
+     return 0;
+    
+}
+// por enquanto, as peças podem acabar saindo do tabuleiro.
